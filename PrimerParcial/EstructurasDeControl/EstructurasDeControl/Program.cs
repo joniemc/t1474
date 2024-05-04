fba7d8dee30efc6596768df530e6784a078cc1b9
jonie_mirc <jonie.mirc@gmail.com>
2024-05-03 19:34:05 -0600

    Ejercicios #1

---
+++ PrimerParcial/EstructurasDeControl/EstructurasDeControl/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EstructurasDeControl {
+    public class Program {
+        public static void Main(string[] args)
+        {
+            double promedio = 0;
+            double sumaAcumulada = 0;
+            Console.Write("Ingrese la cantidad de estudiantes: ");
+            int cantidadEstudiantes = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=0;i<cantidadEstudiantes;i++) {
+                Console.Write("Ingrese la calificación #{0}: ",i+1);
+                double calificacon = Convert.ToDouble(Console.ReadLine());
+                sumaAcumulada += calificacon;
+            }
+
+            promedio = sumaAcumulada/cantidadEstudiantes;
+            Console.WriteLine("El promedio de la sección es: "+promedio);
+
+            Console.ReadKey();
+        }
+    }
+}
